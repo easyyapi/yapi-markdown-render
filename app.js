3b232fe5e13ae8881aad44392d80a1d7fb2e428b
tangcent <sweettom@foxmail.com>
2020-04-30 14:54:13 +0800

    chore: log the request ip.

--- app.js
+++ app.js
@@ -12,8 +12,22 @@ app.use(koaBody({
   }
 ));
 
+const getIp = function (req) {
+  let ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress || req.connection.remoteAddres || '';
+  if (ip.split(',').length > 0) {
+    ip = ip.split(',')[0];
+  }
+  return ip;
+};
+
 router.post('/render', async (ctx, next) => {
   const mdContent = ctx.request.body.toString();
+  try {
+    const ip = getIp(ctx.request);
+    console.log('ip:', ip);
+  } catch (e) {
+    console.log('error to get ip:', e);
+  }
   return ctx.body = toHTML(mdContent);
 });
 
